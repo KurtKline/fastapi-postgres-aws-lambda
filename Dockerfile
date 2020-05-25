51dce2f75c1950a1a96cb5515b6b57f1a99ce21f
Kurt Kline <kurtm.kline@gmail.com>
2020-05-25 21:53:45 +0200

    Removed unnecessary files

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.7
-
-RUN pip install fastapi uvicorn mangum pydantic
-
-EXPOSE 8080
-
-COPY ./example_app /example_app
-
-CMD ["uvicorn", "example_app.main:app", "--host", "0.0.0.0", "--port", "8080"]
--- tests/test_ping.py
+++
@@ -1,9 +0,0 @@
-from starlette.testclient import TestClient
-from example_app.main import app
-
-client = TestClient(app)
-
-
-def test_ping():
-    response = client.get("/ping")
-    assert response.status_code == 200
