3b0906c7396e61d0a1f4459a61582a00871b3a3a
GenuineWolf <47665233+geraudloup@users.noreply.github.com>
2022-03-09 10:42:11 +0100

    fix(ci): test

--- app/main.py
+++ app/main.py
@@ -1,16 +1,7 @@
 from fastapi import FastAPI
-from .routers import players, teams, seasons, refugees
 
-from mangum import Mangum
+app = FastAPI()
 
-app = FastAPI(
-    title="Robota Express API",
-    # openapi_prefix="/prod"
-)
-
-app.include_router(players.router, tags=["Players"])
-app.include_router(teams.router, tags=["Teams"])
-app.include_router(seasons.router, tags=["Seasons"])
-app.include_router(refugees.router, tags=["Seasons"])
-
-# handler = Mangum(app, lifespan="off")
+@app.get("/", tags=["Root"])
+def read_root()-> dict:
+    return {"message": "welcome to FastAPI!"}
