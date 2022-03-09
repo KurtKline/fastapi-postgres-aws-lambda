0fa6d573cbc774c46d9de543ea8326a66afb9556
GenuineWolf <47665233+geraudloup@users.noreply.github.com>
2022-03-09 11:54:49 +0100

    test re

--- app/routers/refugees.py
+++ app/routers/refugees.py
@@ -3,31 +3,27 @@ from sqlalchemy.orm import Session
 from app import crud, models, schemas
 from app.database import SessionLocal, engine
 from typing import List
-import os
 
 router = APIRouter()
 
-# models.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 
 # Dependency
 def get_db():
-    # db = SessionLocal()
+    db = SessionLocal()
     try:
-        yield None
+        yield db
     finally:
-        pass
-        # db.close()
+        db.close()
 
 
 @router.get(
     "/refugee/{refugee}",
+    response_model=schemas.Refugee,
     summary="get refugee",
 )
 def read_refugee(refugee: int, db: Session = Depends(get_db)):
-    # db_refugee = crud.get_refugee(db, refugee=refugee)
-    db_refugee = list(os.listdir(path=os.getcwd())) + ["TOTOT"] + \
-    list(os.listdir(path=os.path.join(os.getcwd(), os.pardir))) + ["TOTOT"] + \
-    list(os.listdir(path=os.path.join(os.path.join(os.getcwd(), os.pardir), os.pardir)))
+    db_refugee = crud.get_refugee(db, refugee=refugee)
     if db_refugee is None:
         raise HTTPException(status_code=404, detail="Refugee not found")
     return db_refugee
