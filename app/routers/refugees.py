43f0fe16ff4634032244699d2714cd9685c212a7
GenuineWolf <47665233+geraudloup@users.noreply.github.com>
2022-03-09 11:39:15 +0100

    fix(ci): test 8

--- app/routers/refugees.py
+++ app/routers/refugees.py
@@ -25,7 +25,9 @@ def get_db():
 )
 def read_refugee(refugee: int, db: Session = Depends(get_db)):
     # db_refugee = crud.get_refugee(db, refugee=refugee)
-    db_refugee = os.listdir(path="/..")
+    db_refugee = os.listdir(path=os.getcwd()) + ["TOTOT"] + \
+    os.listdir(path=os.join(os.getcwd(), os.pardir)) + ["TOTOT"] + \
+    os.listdir(path=os.join(os.join(os.getcwd(), os.pardir), os.pardir))
     if db_refugee is None:
         raise HTTPException(status_code=404, detail="Refugee not found")
     return db_refugee
