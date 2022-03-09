360618927faf8180122200feadf4d522be580783
GenuineWolf <47665233+geraudloup@users.noreply.github.com>
2022-03-09 11:57:22 +0100

    test new

--- app/main.py
+++ app/main.py
@@ -3,10 +3,11 @@ from .routers import refugees
 
 from mangum import Mangum
 
-app = FastAPI()
+app = FastAPI(
+    title="Robota Express API",
+    # openapi_prefix="/prod"
+)
 
-@app.get("/", tags=["Root"])
-def read_root()-> dict:
-    return {"message": "welcome to FastAPI!"}
+app.include_router(refugees.router, tags=["Seasons"])
 
-app.include_router(refugees.router, tags=["Refugees"])
+handler = Mangum(app, lifespan="off")
